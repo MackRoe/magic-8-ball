63cdd699c4ae945ee470af18948945311218a649
MackRoe <elaine.music@students.makeschool.com>
2020-03-27 19:33:13 -0700

    project complete

---
+++ Magic8Ball/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magic8Ball
+//
+//  Created by Elaine Music on 3/26/20.
+//  Copyright © 2020 Dream It Real. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
